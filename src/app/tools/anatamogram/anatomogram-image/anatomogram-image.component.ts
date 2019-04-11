322679232bd27ad04b296d70beb0a4ebf8edc4a3
Tim Sheils <timothy.sheils@nih.gov>
2019-04-11 16:53:16 -0400

    modifying anatamogram programatically

--- src/app/tools/anatamogram/anatomogram-image/anatomogram-image.component.ts
+++ src/app/tools/anatamogram/anatomogram-image/anatomogram-image.component.ts
@@ -1,7 +1,6 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import * as d3 from 'd3';
 import {HttpClient} from "@angular/common/http";
-import {text} from "@angular/core/src/render3";
 
 
 @Component({
@@ -26,6 +25,26 @@ export class AnatomogramImageComponent implements OnInit {
     const svg = d3.select(this.anatamogram.nativeElement).select('svg');
     console.log(svg);
     console.log(svg.classList);
-    this.http.get(this.imageUrl, {responseType: 'text'}).subscribe(res => console.log(res))
+
+    d3.xml(this.imageUrl).then(data => {
+      console.log(data.documentElement);
+      d3.select(this.anatamogram.nativeElement).node().append(data.documentElement)
+     // console.log(svg);
+      console.log("appending");
+    }).then(() => {
+      console.log("doin stuff");
+      d3.select("#UBERON_0000029").selectAll('path').style('fill', 'green');
+    //  d3.select("#UBERON_0000029").attr('class', 'filled');
+      console.log(d3.select("#UBERON_0000029").node());
+      // console.log(d3.selectAll("#UBERON_0000956").node());
+    }
+  )
+
+/*    this.http.get(this.imageUrl, {responseType: 'text'}).subscribe(res => {
+      console.log(res)
+      console.log(d3.select("#UBERON_0000956"));
+      console.log(d3.select("#UBERON_0000956").node().attribute(":inkscape:label"));
+
+    })*/
   }
 }
