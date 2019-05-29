26ace231227ebe0b8aec793c03675945e9d80a43
Tim Sheils <timothy.sheils@nih.gov>
2019-05-29 17:10:26 -0400

    fix api page tests

--- src/app/api-page/api-page.component.spec.ts
+++ src/app/api-page/api-page.component.spec.ts
@@ -3,18 +3,7 @@ import {ApiPageComponent} from './api-page.component';
 import {SharedModule} from '../shared/shared.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from '../app-routing.module';
-<<<<<<< HEAD
-=======
-import {DataTypesPanelComponent} from '../pharos-home/data-types-panel/data-types-panel.component';
-import {AboutPanelComponent} from '../pharos-home/about-panel/about-panel.component';
-import {PathResolverService} from '../pharos-services/path-resolver.service';
-import {FaqPageComponent} from '../faq-page/faq-page.component';
-import {NewsPanelComponent} from '../pharos-home/news-panel/news-panel.component';
-import {AboutPageComponent} from '../about-page/about-page.component';
->>>>>>> deploy
-import {LoadingService} from '../pharos-services/loading.service';
-import {ApiViewerComponent} from '../tools/api-viewer/api-viewer.component';
-import {APP_BASE_HREF} from '@angular/common';
+import {APP_BASE_HREF} from "@angular/common";
 
 
 describe('ApiPageComponent', () => {
@@ -29,20 +18,9 @@ describe('ApiPageComponent', () => {
         AppRoutingModule
       ],
       declarations: [
-<<<<<<< HEAD
-=======
-        FaqPageComponent,
->>>>>>> deploy
         ApiPageComponent,
-        ApiViewerComponent
       ],
       providers: [
-<<<<<<< HEAD
-=======
-        PharosApiService,
-        PathResolverService,
->>>>>>> deploy
-        LoadingService,
         {provide: APP_BASE_HREF, useValue: '/index' }
       ]
     })
