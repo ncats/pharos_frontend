4d8bee804e28141f0f13415df01e326aa83b83e6
Tim Sheils <timothy.sheils@nih.gov>
2020-02-06 17:48:49 -0500

    working on scrollTo functionality

--- src/app/tools/custom-content.directive.ts
+++ src/app/tools/custom-content.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ViewContainerRef} from '@angular/core';
+import {Directive, Inject, ViewContainerRef} from '@angular/core';
 
 /**
  * Directive to return a view container reference instead of using a view child
@@ -12,7 +12,14 @@ export class CustomContentDirective {
   /**
    * sets a view container that the parent component can interact with
    * @param {ViewContainerRef} viewContainerRef
+   * @param shared
    */
-    constructor(public viewContainerRef: ViewContainerRef) { }
+    constructor(
+      public viewContainerRef: ViewContainerRef,
+      @Inject('AppComponentService') shared
+  ) {
+      console.log(this);
+   //   shared.register(viewContainerRef);
+  }
 
 }
