c92135c4cc0a00a393d2d829eba44e45f4bb5ac5
Keith Kelleher <keith.kelleher@nih.gov>
2020-03-13 09:27:30 -0400

    sync to base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,13 +17,14 @@ export const environment = {
   production: true,
   host: _HOST,
   api: _API,
+  graphqlUrl: 'https://pharos-api.ncats.io/graphql',
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyAT2FJEc7Ju_yqwZ8xhEK9HBbyc010th78',
+    authDomain: 'ncatsidg.firebaseapp.com',
+    databaseURL: 'https://ncatsidg.firebaseio.com',
+    projectId: 'ncatsidg',
+    storageBucket: 'ncatsidg.appspot.com',
+    messagingSenderId: '511313064782'
   }
 };
 
