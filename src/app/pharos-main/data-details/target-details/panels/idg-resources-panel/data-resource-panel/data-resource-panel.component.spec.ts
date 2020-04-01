40498d7b4438e03491bfeae6c2a8f8c59d5faef6
Keith Kelleher <keith.kelleher@nih.gov>
2020-04-01 15:25:54 -0400

    Add tests for new components

--- src/app/pharos-main/data-details/target-details/panels/idg-resources-panel/data-resource-panel/data-resource-panel.component.spec.ts
+++ src/app/pharos-main/data-details/target-details/panels/idg-resources-panel/data-resource-panel/data-resource-panel.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DataResourcePanelComponent } from './data-resource-panel.component';
+import * as test_data from "../../../../../../../../test/test-data-sources";
+import * as dataResources from "../../../../../../models/idg-resources/data-resource";
 
 describe('DataResourcePanelComponent', () => {
   let component: DataResourcePanelComponent;
@@ -15,11 +16,21 @@ describe('DataResourcePanelComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DataResourcePanelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component = fixture.componentInstance;
+    component.dataResource = test_data.TEST_RESOURCE_NANOBRET1;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should be the right resource types', () => {
+    expect(test_data.TEST_RESOURCE_EXPRESSION1 instanceof dataResources.Dataset).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_EXPRESSION2 instanceof dataResources.Dataset).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_MOUSE_IMAGING1 instanceof dataResources.MouseImageData).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_MOUSE_IMAGING2 instanceof dataResources.MouseImageData).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_NANOBRET1 instanceof dataResources.Dataset).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_NANOBRET2 instanceof dataResources.Dataset).toBeTruthy();
   });
 });
--- src/app/pharos-main/data-details/target-details/panels/idg-resources-panel/reagent-panel/reagent-panel.component.spec.ts
+++ src/app/pharos-main/data-details/target-details/panels/idg-resources-panel/reagent-panel/reagent-panel.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ReagentPanelComponent } from './reagent-panel.component';
+import * as test_data from "../../../../../../../../test/test-data-sources";
+import * as reagents from "../../../../../../models/idg-resources/reagent";
 
 describe('ReagentPanelComponent', () => {
   let component: ReagentPanelComponent;
@@ -15,11 +16,25 @@ describe('ReagentPanelComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ReagentPanelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component = fixture.componentInstance;
+    component.reagent = test_data.TEST_RESOURCE_CELL1;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should be the right resource types', () => {
+    expect(test_data.TEST_RESOURCE_CELL1 instanceof reagents.Cell).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_CELL2 instanceof reagents.Cell).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_CHEMICAL_TOOL1 instanceof reagents.SmallMolecule).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_CHEMICAL_TOOL2 instanceof reagents.SmallMolecule).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_CONSTRUCT1 instanceof reagents.GeneticConstruct).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_CONSTRUCT2 instanceof reagents.GeneticConstruct).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_MOUSE1 instanceof reagents.Mouse).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_MOUSE2 instanceof reagents.Mouse).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_PEPTIDE1 instanceof reagents.Peptide).toBeTruthy();
+    expect(test_data.TEST_RESOURCE_PEPTIDE1 instanceof reagents.Peptide).toBeTruthy();
   });
 });
