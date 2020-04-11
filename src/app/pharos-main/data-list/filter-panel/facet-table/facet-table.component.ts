8906a02d04c1c51cb70b56f8ba1af1b6b942e04c
Keith Kelleher <keith.kelleher@nih.gov>
2020-04-11 00:00:51 -0400

    add search box to filter facet options based on a search string

--- src/app/pharos-main/data-list/filter-panel/facet-table/facet-table.component.ts
+++ src/app/pharos-main/data-list/filter-panel/facet-table/facet-table.component.ts
@@ -1,12 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-  ViewEncapsulation
-} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {SelectionModel} from '@angular/cdk/collections';
@@ -16,6 +8,7 @@ import {Subject} from 'rxjs';
 import {PathResolverService} from '../path-resolver.service';
 import {SelectedFacetService} from '../selected-facet.service';
 import {PharosApiService} from "../../../../pharos-services/pharos-api.service";
+import {HighlightPipe} from "../../../../tools/search-component/highlight.pipe";
 
 /**
  * table to display selectable fields
@@ -41,6 +34,10 @@ export class FacetTableComponent implements OnInit, OnDestroy {
    */
   dataSource = new MatTableDataSource<any>([]);
 
+  /**
+   * data source to show filtered results based on text search
+   */
+  filteredDataSource = new MatTableDataSource<any>([]);
   /**
    * selection model to track selected filters
    * @type {SelectionModel<string>}
@@ -76,6 +73,16 @@ export class FacetTableComponent implements OnInit, OnDestroy {
   propogate = true;
 
   /**
+   * the text string what someone is using to find facet options
+   */
+  searchText: string = "";
+
+  /**
+   * helps highlight the search terms. included as object since I couldn't get the module to share to use the pipe directly :(
+   */
+  private highlight: HighlightPipe = new HighlightPipe();
+
+  /**
    * add route and change tracking dependencies
    * @param _route
    * @param router
@@ -102,6 +109,7 @@ export class FacetTableComponent implements OnInit, OnDestroy {
         if (e instanceof NavigationEnd) {
           // update field values
           this.dataSource.data = this.facet.values;
+          this.populateFilteredData();
           // update selected fields
           this.mapSelected();
           this.changeRef.markForCheck();
@@ -109,6 +117,7 @@ export class FacetTableComponent implements OnInit, OnDestroy {
       });
     // update field values
     this.dataSource.data = this.facet.values;
+    this.populateFilteredData();
     // update selected fields
     this.mapSelected();
 
@@ -148,11 +157,22 @@ export class FacetTableComponent implements OnInit, OnDestroy {
    * @param event
    */
   scrollDetected(event) {
-    if (event.target.scrollHeight - event.target.offsetHeight - event.target.scrollTop <= 5 ) {
+    if (event.target.scrollHeight - event.target.offsetHeight - event.target.scrollTop <= 5) {
       if (this.facet.values.length < this.facet.count) {
         this.fetchAllFilterOptions();
       }
     }
+  }
+
+  /**
+   * show underline of the searchText
+   * @param rowText
+   */
+  highlightText(rowText: string){
+    if(this.searchText.length == 0) {
+      return rowText;
+    }
+    return this.highlight.transform(rowText,this.searchText);
   }
 
   /**
@@ -164,15 +184,18 @@ export class FacetTableComponent implements OnInit, OnDestroy {
       this._route.snapshot.data.path,
       this._route.snapshot.queryParamMap,
       this.facet.facet,
-      this.facet.count).subscribe({next:
-      res => {
-        this.facet = res.data.results.facets.find(resfacet => resfacet.facet == this.facet.facet);
-        this.dataSource.data = this.facet.values;
-        this.mapSelected();
-        this.loading = false;
-      },error: e=>{
+      this.facet.count).subscribe({
+      next:
+        res => {
+          this.facet = res.data.results.facets.find(resfacet => resfacet.facet == this.facet.facet);
+          this.dataSource.data = this.facet.values;
+          this.populateFilteredData();
+          this.mapSelected();
+          this.loading = false;
+        }, error: e => {
         throw(e);
-      }});
+      }
+    });
   }
 
   /**
@@ -186,14 +209,30 @@ export class FacetTableComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * filter facets by query
-   * todo implement this
-   * @param {string} q
+   * filter facet options by searchText
+   * @param {string} searchText
    */
-  filterFacet(q: string): void {
-    // console.log(q);
+  filterFacet(searchText: string): void {
+    this.searchText = searchText;
+    if (searchText.length > 0 && this.facet.values.length < this.facet.count) {
+      this.fetchAllFilterOptions();
+    }
+    this.populateFilteredData();
   }
 
+  /**
+   * populates the filteredDataSource that is being shown
+   */
+  private populateFilteredData() {
+    if (this.searchText.length == 0) {
+      this.filteredDataSource.data = this.dataSource.data;
+      return;
+    }
+    this.filteredDataSource.data = this.dataSource.data.filter(row => {
+      let rowname = (row.value || row.name).toLowerCase();
+      return rowname.indexOf(this.searchText.toLowerCase()) > -1;
+    });
+  }
 
   /**
    * function to unubscribe on destroy
