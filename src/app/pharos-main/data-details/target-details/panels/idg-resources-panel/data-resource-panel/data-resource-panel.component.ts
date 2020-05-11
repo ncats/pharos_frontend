ea4da3330c8157ff0c43e60a89852de8416af208
Keith Kelleher <keith.kelleher@nih.gov>
2020-05-11 17:28:09 -0400

    Fix <li> properties in Firefox

--- src/app/pharos-main/data-details/target-details/panels/idg-resources-panel/data-resource-panel/data-resource-panel.component.ts
+++ src/app/pharos-main/data-details/target-details/panels/idg-resources-panel/data-resource-panel/data-resource-panel.component.ts
@@ -7,7 +7,7 @@ import {DataResource} from "../../../../../../models/idg-resources/data-resource
 @Component({
   selector: 'pharos-data-resource-panel',
   templateUrl: './data-resource-panel.component.html',
-  styleUrls: ['./data-resource-panel.component.scss', '../reagent-panel/reagent-panel.component.scss']
+  styleUrls: ['../reagent-panel/reagent-panel.component.scss']
 })
 export class DataResourcePanelComponent implements OnInit {
 
