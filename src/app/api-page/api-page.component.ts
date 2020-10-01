c4c100270b6f05de47bab40ffadfbf488bfdac0d
Keith Kelleher <keith.kelleher@nih.gov>
2020-10-01 17:45:28 -0400

    Example queries for the API page

--- src/app/api-page/api-page.component.ts
+++ src/app/api-page/api-page.component.ts
@@ -14,26 +14,135 @@ import {environment} from "../../environments/environment";
   styleUrls: ['./api-page.component.scss']
 })
 
-
-export class ApiPageComponent implements OnInit{
+export class ApiPageComponent implements OnInit {
   /**
    * no args constructor
    */
   constructor(private sanitizer: DomSanitizer,
               public dialog: MatDialog,
               @Inject(PLATFORM_ID) private platformID: Object,
-              private metaService: UnfurlingMetaService){}
+              private metaService: UnfurlingMetaService) {
+  }
+
   openDialog() {
     const dialogRef = this.dialog.open(ApiHelpComponent);
 
     dialogRef.afterClosed().subscribe(result => {
+      this.changeQuery(this.queryMap.values().next().value);
     });
   }
 
-  url = this.sanitizer.bypassSecurityTrustResourceUrl(environment.graphqlUrl);
+  query: QueryDetails;
+  previousquery: QueryDetails;
+  queryInFrame: number;
 
-  ngOnInit(){
-    if(isPlatformBrowser(this.platformID)) {
+  url() {
+    if(!this.query || this.query.key === this.queryInFrame){
+      return;
+    }
+    this.queryInFrame = this.query.key;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(environment.graphqlUrl + `?query=${encodeURIComponent(this.query.query)}`);
+  }
+
+  isActive(key: number) {
+    return key === this.queryInFrame;
+  }
+
+  queryMap: Map<number, QueryDetails> = new Map<number, QueryDetails>([
+    [0,
+      {
+        name: "Target details",
+        key: 0,
+        description: "Given a gene symbol, this query fetches a number of simple details for the target.",
+        query: QueryDetails.simpleTargetDetails
+      }
+    ],
+    [1,
+      {
+        name: "Disease details",
+        key: 1,
+        description: "Given a disease name, this query fetches some basic information about a disease, and one " +
+          "level of descendents, based on the Disease Ontology hierarchy",
+        query: QueryDetails.simpleDiseaseDetails
+      }
+    ],
+    [2,
+      {
+        name: "Ligand details",
+        key: 2,
+        description: "Given a drug name, this query fetches some basic information about the drug " +
+          "and details of target activity measurements.",
+        query: QueryDetails.simpleLigandDetails
+      }
+      ],
+    [3,
+      {
+        name: "Protein-protein interactions",
+        key: 3,
+        description: "Given a target symbol, this query fetches some basic details of the top 5 interacting proteins, and details" +
+          " of that interaction.",
+        query: QueryDetails.interactingProteins
+      }],
+    [4,
+      {
+        name: "Disease associations",
+        key: 4,
+        description: "There are two queries given here. Given a disease name, the first query fetches the top 5 assoc" +
+          "iated targets, and details of that association. Given a target symbol, the second query fetches associated " +
+          "diseasese. (You may need to click 'prettify' to execute both queries)",
+        query: QueryDetails.associatedDiseases
+      }],
+    [5,
+      {
+        name: "Facets for all targets",
+        key: 5,
+        description: "Fetches the default facets for all targets in TCRD.",
+        query: QueryDetails.facetsForAllTargets
+      }],
+    [6,
+      {
+        name: "Facets for an associated disease",
+        key: 6,
+        description: "Fetches the default facets for all targets associated with a given disease.",
+        query: QueryDetails.facetsForTargetsForDisease
+      }],
+    [7,
+      {
+        name: "Filtering by facet values",
+        key: 7,
+        description: "Fetching the default facets, filtering by facet values. Note facets only filter the target list as " +
+          "it applies to the other facets, not themselves.",
+        query: QueryDetails.filteringByFacets
+      }],
+    [8,
+      {
+        name: "Fetching uncommon facets",
+        key: 8,
+        description: "Uncommon facets can be fetched thusly.",
+        query: QueryDetails.fetchingUncommonFacets
+      }],
+    [9,
+      {
+        name: "All Facet Names",
+        key: 9,
+        description: "All facets that are currently supported in a target list.",
+        query: QueryDetails.getAllTargetFacets
+      }]
+  ]);
+
+
+
+
+
+
+
+
+  changeQuery(query: QueryDetails) {
+    this.query = query;
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformID)) {
       this.openDialog();
     }
     let newDescription = 'Build and run queries on the Pharos GraphQL server.';
@@ -47,5 +156,166 @@ export class ApiPageComponent implements OnInit{
   templateUrl: './api-help.component.html',
   styleUrls: ['./api-help.component.scss']
 })
+export class ApiHelpComponent {
+}
 
-export class ApiHelpComponent {}
+export class QueryDetails {
+  key: number;
+  query: string;
+  name: string;
+  description: string;
+
+  static simpleTargetDetails = `
+query targetDetails{
+  target(q:{sym:"ACE2"}) {
+    name
+    tdl
+    fam
+    sym
+    description
+    novelty
+  }
+}`;
+
+  static simpleDiseaseDetails = `
+query diseaseDetails{
+  disease(name:"asthma"){
+    name
+    uniprotDescription
+    doDescription
+    targetCounts{
+      name
+      value
+    }
+    children{
+      name
+      doDescription
+    }
+  }
+}`;
+
+  static simpleLigandDetails = `
+query ligandDetails{
+  ligand(ligid: "haloperidol") {
+    name
+    description
+    isdrug
+    synonyms {
+      name
+      value
+    }
+    smiles
+    activities {
+      target {
+        sym
+      }
+      type
+      value
+    }
+  }
+}`;
+
+  static interactingProteins = `
+query interactingProteins{
+  targets(filter: { associatedTarget: "CAMKK1" }) {
+    targets(top:5) {
+      name
+      sym
+      ppiTargetInteractionDetails {
+        dataSources:ppitypes
+        score
+        interaction_type
+        evidence
+        p_ni
+        p_int
+        p_wrong
+      }
+    }
+  }
+}`;
+
+  static associatedDiseases = `
+query associatedTargets{
+  targets(filter: { associatedDisease: "asthma" }) {
+    targets(top: 5) {
+      name
+      sym
+      diseaseAssociationDetails {
+        name
+        dataType
+        evidence
+      }
+    }
+  }
+}
+query associatedDiseases{
+  diseases(filter:{associatedTarget:"ORMDL3"}){
+    diseases{
+      name
+    }
+  }
+}`;
+
+static facetsForAllTargets = `
+query facetsForAllTargets {
+  targets {
+    facets {
+      facet
+      values {
+        name
+        value
+      }
+    }
+  }
+}`;
+
+static facetsForTargetsForDisease = `
+query facetsForTargetsForDisease {
+  targets(filter: { associatedDisease: "asthma" }) {
+    facets {
+      facet
+      dataType
+      values {
+        name
+        value
+      }
+    }
+  }
+}`;
+static filteringByFacets = `
+query filteringByFacets {
+  targets(
+    filter: {
+      facets: [{
+        facet: "Target Development Level",
+        values: ["Tclin", "Tchem"]
+      }]
+    }
+  ) {
+    facets {
+      facet
+      values {
+        name
+        value
+      }
+    }
+  }
+}`;
+static fetchingUncommonFacets = `
+query fetchingUncommonFacets{
+  targets(facets:["Ortholog"]){
+    facets{
+      facet
+      values{
+        name
+        value
+      }
+    }
+  }
+}`;
+
+static getAllTargetFacets = `
+query getAllTargetFacets{
+  targetFacets
+}`;
+}
