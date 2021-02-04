3f44ae1e7241261b2e3a7a21155cf71b9e6a6853
Keith Kelleher <keith.kelleher@nih.gov>
2021-02-04 15:31:49 -0500

    use redirector for rendering structures

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
 /**
  * environment object to set basic urls and firebase configuration
  */
+const backend = 'https://pharos-api.ncats.io';
+
 export const environment = {
+  rendererUrl: `${backend}/render`,
   production: true,
-  graphqlUrl: 'https://pharos-api.ncats.io/graphql',
+  graphqlUrl: `${backend}/graphql`,
   firebase: {
     apiKey: '',
     authDomain: '',
