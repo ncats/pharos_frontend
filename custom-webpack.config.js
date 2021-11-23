c15989b87da0d339aaee9c267dfc4c117fd9a0a6
KeithKelleher <keith.kelleher@ncats.nih.gov>
2021-11-23 14:20:09 -0500

    fix viewer styling

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -4,9 +4,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.svg$/,
+        test: /\.svg$/i,
         loader: 'svg-inline-loader',
-      }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ]
   }
 };
