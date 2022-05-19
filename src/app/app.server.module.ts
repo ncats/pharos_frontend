b2f82ad8e6576594eaa60225ab11f44c9f3692c4
KeithKelleher <keith.kelleher@ncats.nih.gov>
2022-05-19 14:40:25 -0400

    minor fixes to styling for SSR / prerendered pages

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,8 +4,6 @@ import {ServerModule, ServerTransferStateModule} from '@angular/platform-server'
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import {FlexLayoutServerModule} from '@angular/flex-layout/server';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   imports: [
