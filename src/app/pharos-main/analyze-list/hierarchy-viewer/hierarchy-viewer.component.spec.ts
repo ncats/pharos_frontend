9be3a4a4f40535654744a6e4ab853154e4ea6225
KeithKelleher <keith.kelleher@ncats.nih.gov>
2022-09-20 14:47:59 -0400

    fix unit tests

--- src/app/pharos-main/analyze-list/hierarchy-viewer/hierarchy-viewer.component.spec.ts
+++ src/app/pharos-main/analyze-list/hierarchy-viewer/hierarchy-viewer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HierarchyViewerComponent } from './hierarchy-viewer.component';
+import {ActivatedRoute} from "@angular/router";
+import {MOCKACTIVATEDROUTE} from "../../../../../test/mock-activate-route";
 
 describe('HierarchyViewerComponent', () => {
   let component: HierarchyViewerComponent;
@@ -8,7 +10,10 @@ describe('HierarchyViewerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HierarchyViewerComponent ]
+      declarations: [ HierarchyViewerComponent ],
+      providers: [
+        {provide: ActivatedRoute, useValue: MOCKACTIVATEDROUTE}
+      ]
     })
     .compileComponents();
   });
--- src/app/pharos-main/data-details/target-details/panels/disease-novelty/disease-novelty.component.spec.ts
+++ src/app/pharos-main/data-details/target-details/panels/disease-novelty/disease-novelty.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiseaseNoveltyComponent } from './disease-novelty.component';
+import {ActivatedRoute} from "@angular/router";
+import {MOCKACTIVATEDROUTE} from "../../../../../../../test/mock-activate-route";
 
 describe('DiseaseNoveltyComponent', () => {
   let component: DiseaseNoveltyComponent;
@@ -8,7 +10,10 @@ describe('DiseaseNoveltyComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DiseaseNoveltyComponent ]
+      declarations: [ DiseaseNoveltyComponent ],
+      providers: [
+        {provide: ActivatedRoute, useValue: MOCKACTIVATEDROUTE}
+      ]
     })
     .compileComponents();
   });
